4726ded7306c7e78872e0284ff7c39bb88c48ef2
Captain-Ironbump <linde.lukas.99@gmail.com>
2024-02-14 01:41:27 +0100

    opengl with window initialization (first fake, then real)

--- src/main.cpp
+++ src/main.cpp
@@ -1,92 +1,14 @@
 #include "main_lib.h"
+#include "platform.h"
 
-// #############################################################################################################
-//                              Platform Globals
-// #############################################################################################################
+#define APIENTRY
+#include "glcorearb.h"
 
-static bool running = true;
-
-// #############################################################################################################
-//                              Platform Functions
-// #############################################################################################################
-bool platform_create_window(int width, int height, char* title);
-void platform_update_window();
-// #############################################################################################################
-//                              Windows Platform
-// #############################################################################################################
 #ifdef _WIN32
-#define WIN32_LEAN_AND_MEAN
-#define NOMINMAX
-#include <windows.h>
-
-
-// #############################################################################################################
-//                              Windows Globals
-// #############################################################################################################
-static HWND window;
-
-// #############################################################################################################
-//                              Platform Implementations
-// #############################################################################################################
-LRESULT CALLBACK windows_window_callback(HWND window, UINT msg, WPARAM wParam, LPARAM lParam)
-{
-    LRESULT result = 0;
-
-    switch (msg)
-    {
-    case WM_CLOSE:
-        running = false;
-        break;
-    
-    default:
-        result = DefWindowProcA(window, msg, wParam, lParam);
-        break;
-    }
-    return result;
-}
-
-bool platform_create_window(int width, int height, char* title) 
-{
-    HINSTANCE instance = GetModuleHandleA(0);
-
-    WNDCLASSA wc = {};
-    wc.hInstance = instance;
-    wc.hIcon = LoadIcon(instance, IDI_APPLICATION);
-    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-    wc.lpszClassName = title; // This is not the title, just a unique Identifier(ID)
-    wc.lpfnWndProc = windows_window_callback;
-
-    if (!RegisterClassA(&wc))
-    {
-        return false;
-    }
-
-    int dwStyle = WS_OVERLAPPEDWINDOW;
-
-    window = CreateWindowExA(0, title, title, dwStyle, 100, 100, width, height, NULL, NULL, instance, NULL);
-
-    if (window == NULL)
-    {
-        return false;
-    }
-
-    ShowWindow(window, SW_SHOW);
-    return true;
-}
-
-void platform_update_window()
-{
-    MSG msg;
-
-    while (PeekMessageA(&msg, window, 0, 0, PM_REMOVE))
-    {
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
-    }
-}
-
-
+#include "win32_platform.cpp"
 #endif
+
+#include "gl_renderer.h"
 
 
 int main() 
@@ -97,10 +19,6 @@ int main()
         // Update
         platform_update_window();
 
-        SM_TRACE("TEST");
-        SM_WARN("Hooo");
-        SM_ERROR("NO");
-        SM_ASSERT(false, "assertion hit");
     }
     return 0;
 }
